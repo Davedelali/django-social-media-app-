c8d8b86c5b96aafab5883bed876fe10d907cfd98
Davedelali <daganya922@gmail.com>
2024-02-24 23:05:49 +0700

    registration

--- account/urls.py
+++ account/urls.py
@@ -9,5 +9,27 @@ urlpatterns = [
   path('login/', auth_views.LoginView.as_view(), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
   
+  #change password views
+  path('password-change/',
+       auth_views.PasswordChangeView.as_view(),
+       name='password_change'),
+  path('password-change/done/',
+       auth_views.PasswordChangeDoneView.as_view(),
+       name='password_change_done'), 
+  
+  #reset password urls
+  path('passwrod-reset/',
+       auth_views.PasswordResetView.as_view(),
+       name='password_reset'),
+  path('password-reset/done/',
+       auth_views.PasswordResetDoneView.as_view(),
+       name='password_reset_done'),
+  path('password-reset/<uidb64>/<token>',
+       auth_views.PasswordResetConfirmView.as_view(),
+       name='password_reset_confirm'),
+  path('password_reset_complete/',
+       auth_views.PasswordResetCompleteView.as_view(),
+       name='password_reset_complete'),
+  
   path('', views.dashboard, name='dashboard'),
 ]
